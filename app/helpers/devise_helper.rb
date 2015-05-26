4c23a0e8e1e903119fec85e7b444b95830d6a41e
Sebastian <juseve200@gmail.com>
2015-05-26 11:25:54 -0500

    Added language change option

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,23 @@
+module DeviseHelper
+  # A simple way to show error messages for the current devise resource. If you need
+  # to customize this method, you can either overwrite it in your application helpers or
+  # copy the views to your application.
+  #
+  # This method is intended to stay simple and it is unlikely that we are going to change
+  # it to add more behavior or options.
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:div, msg) }.join
+
+    html = <<-HTML
+      <div class="alert alert-danger alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span></button>
+        #{messages}
+      </div>
+    HTML
+
+    html.html_safe
+  end
+end
